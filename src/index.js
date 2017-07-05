b8c98596250e8f5f08ff413294c95a636287d67b
Pak Au <pakchungau@gmail.com>
2017-07-05 22:31:22 +1000

    Added React Router.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import SearchBar from './components/SearchBar'
 import './index.css'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path='/' exact component={App} />
+      <Route path='/search' component={SearchBar} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root'))
