a08ef321d634cd08340aaeb0f69c30f51bc5ad3a
Pak Au <pakchungau@gmail.com>
2017-07-23 15:24:14 +1000

    Refactor project.

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react'
-import Selector from '../Selector'
+import React, { Component } from "react";
+import Selector from "../Selector";
 
-const Book = (props) => (
-  <div className="book">
-    <div className="book-top">
-      <div className="book-cover" style={{
-        width: props.width,
-        height: props.height,
-        backgroundImage: `url(${props.imageUrl})` }}></div>
-      <Selector />
-    </div>
-    <div className="book-title">{props.title}</div>
-    <div className="book-authors">{props.authors}</div>
-  </div>
-)
+const Book = props =>
+  // <div className="book">
+  //   <div className="book-top">
+  //     <div className="book-cover" style={{
+  //       width: props.width,
+  //       height: props.height,
+  //       backgroundImage: `url(${props.imageUrl})` }}></div>
+  //     <Selector />
+  //   </div>
+  //   <div className="book-title">{props.title}</div>
+  //   <div className="book-authors">{props.authors}</div>
+  // </div>
+  <div>Book</div>;
 
-export default Book
+export default Book;
---
+++ src/components/BookShelf/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Book from "../Book/index.js";
+
+const BookShelf = props =>
+  <div>
+    BookShelf
+    <Book />
+  </div>;
+
+export default BookShelf;
