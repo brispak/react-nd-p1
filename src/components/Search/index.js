ebf8c1122823b8b85ef439fc4396a30ee764b079
Pak Au <pakchungau@gmail.com>
2017-08-06 10:19:44 +1000

    import proptypes

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Book from "../Book/index.js";
 import * as BooksAPI from "../../BooksAPI.js";
+import PropTypes from "prop-types";
 import BookShelf from "../BookShelf/index.js";
 
 class Search extends Component {
